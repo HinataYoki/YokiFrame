51bfe4697ab7fd50fe3a7b0ef490a4aff366c0f0
hinatayoki-admin <hinatayoki@qq.com>
2025-05-14 20:52:32 +0800

    非Mono单例创建由 非公开构造函数 变为 无参构造函数

--- YokiFrame/Core/Kit/SingletonKit/Creator/SingletonCreator.cs
+++ YokiFrame/Core/Kit/SingletonKit/Creator/SingletonCreator.cs
@@ -23,7 +23,7 @@ namespace YokiFrame
             }
             else
             {
-                var instance = CreateNonPublicConstructorObject<T>();
+                var instance = CreateNonArgsConstructorObject<T>();
                 instance.OnSingletonInit();
                 return instance;
             }
@@ -73,18 +73,18 @@ namespace YokiFrame
             return instance;
         }
 
-        private static T CreateNonPublicConstructorObject<T>() where T : class
+        private static T CreateNonArgsConstructorObject<T>() where T : class
         {
             var type = typeof(T);
-            // 获取私有构造函数
-            var constructorInfos = type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+            // 获取构造函数
+            var constructorInfos = type.GetConstructors();
 
             // 获取无参构造函数
             var ctor = Array.Find(constructorInfos, c => c.GetParameters().Length == 0);
 
             return ctor switch
             {
-                null => throw new Exception($"Non-Public Constructor() not found! in {type}"),
+                null => throw new Exception($"Non-Args Constructor() not found! in {type}"),
                 _ => ctor.Invoke(null) as T
             };
         }
