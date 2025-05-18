d61819efa3489f5316b8b7f3cafe8f8662fc2c25
SugarKokomi <2475945798@qq.com>
2025-05-18 11:49:39 +0800

    增加两处拓展。拓展了Transform及其Vector3的部分方法

---
+++ YokiFrame/Core/Kit/FluentApi/TransformExtension.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace YokiFrame
+{
+    public static class TransformExtension
+    {
+        /// <summary> 通过名字查找指定名字的子物体，然后获取挂载的Component </summary>
+        /// <param name="targetName">物体的名字</param>
+        /// <returns>返回找到的该物体的组件T，没有找到则返回null</returns>
+        public static T FindComponent<T>(this GameObject parent, string targetName) where T : Component => parent.transform.FindComponent<T>(targetName);
+        /// <summary> 通过名字查找指定名字的子物体，然后获取挂载的Component </summary>
+        /// <param name="targetName">物体的名字</param>
+        /// <returns>返回找到的该物体的组件T，没有找到则返回null</returns>
+        public static T FindComponent<T>(this Transform parent, string targetName) where T : Component
+        {
+            if (parent.name == targetName && parent.TryGetComponent<T>(out var component))
+                return component;
+            foreach (Transform child in parent)
+            {
+                T result = child.FindComponent<T>(targetName);
+                if (result) return result;
+            }
+            return null;
+        }
+        /// <summary> 在局部空间中，复位Transform到默认位置 </summary>
+        public static void ResetTransform(this Transform self, bool resetScale = true)
+        {
+            self.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+            if (resetScale)
+                self.localScale = Vector3.one;
+        }
+    }
+}
