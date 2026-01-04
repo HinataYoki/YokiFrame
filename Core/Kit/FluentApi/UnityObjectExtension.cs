fb398859b241f1143864918197897b8bebdf27b8
yoki <hinatayoki@qq.com>
2026-01-05 03:33:47 +0800

    FluentApi：更新常用方法调用

--- Core/Kit/FluentApi/UnityObjectExtension.cs
+++ Core/Kit/FluentApi/UnityObjectExtension.cs
@@ -27,5 +27,22 @@ namespace YokiFrame
             self.transform.parent = parent.transform;
             return self.gameObject;
         }
+
+        /// <summary>
+        /// 获取或添加组件（通用扩展方法）
+        /// </summary>
+        public static T GetOrAddComponent<T>(this GameObject self) where T : Component
+        {
+            var comp = self.GetComponent<T>();
+            return comp ? comp : self.AddComponent<T>();
+        }
+
+        /// <summary>
+        /// 获取或添加组件（MonoBehaviour 扩展）
+        /// </summary>
+        public static T GetOrAddComponent<T>(this MonoBehaviour self) where T : Component
+        {
+            return self.gameObject.GetOrAddComponent<T>();
+        }
     }
 }
