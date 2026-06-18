2c1306fa5d36a951419323514ed2f295f66da2d1
HinataYoki <hinatayoki@qq.com>
2026-06-18 21:22:36 +0800

    feat(Tools): 移除 NodeKit
    NodeKit 尚未达到生产环境标准，从 Tools 层移除。

--- Tools/NodeKit/Examples/ExampleNodeGraph.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-namespace YokiFrame.NodeKit.Examples
-{
-    [CreateAssetMenu(fileName = "ExampleNodeGraph", menuName = "YokiFrame/NodeKit/Example Graph")]
-    public class ExampleNodeGraph : NodeGraph { }
-}
